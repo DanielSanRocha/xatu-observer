7fd44fffb4d357ece55b9b7d5f8719c3f9c40ac6
Daniel Santana Rocha <danielsantanarocha@gmail.com>
2024-11-16 15:43:05 -0300

    Skipping not found directories.

--- src/main/scala/com/danielsanrocha/xatu/observers/LogServiceObserver.scala
+++ src/main/scala/com/danielsanrocha/xatu/observers/LogServiceObserver.scala
@@ -20,45 +20,50 @@ class LogServiceObserver(s: Service, implicit val service: ServiceService, impli
 
     val regex = raw"${_data.logFileRegex}".r
     val directoryPath = new File(_data.logFileDirectory)
-    try {
-      directoryPath.list() foreach { filename =>
-        if (regex matches filename) {
-          logging.debug(s"File $filename match regex! Observing it...")
+    val fs = directoryPath.list()
+    if (fs == null) {
+      logging.warn(s"Directory ${_data.logFileDirectory} not found, skipping!")
+    } else {
+      try {
+        fs foreach { filename =>
+          if (regex matches filename) {
+            logging.debug(s"File $filename match regex! Observing it...")
 
-          val b: BufferedReader = files.get(filename) match {
-            case Some(bufferedReader) =>
-              logging.debug(s"already listening to file ${filename}")
-              bufferedReader
+            val b: BufferedReader = files.get(filename) match {
+              case Some(bufferedReader) =>
+                logging.debug(s"already listening to file ${filename}")
+                bufferedReader
 
-            case None =>
-              logging.debug(s"creating buffered reader to file ${filename}")
+              case None =>
+                logging.debug(s"creating buffered reader to file ${filename}")
 
-              val fullpath = Paths.get(_data.logFileDirectory, filename)
-              val bufferedReader = new BufferedReader(new FileReader(fullpath.toFile))
-              files.addOne(filename -> bufferedReader)
-              var line: String = ""
-              while (line != null) {
-                line = bufferedReader.readLine()
-              }
-              bufferedReader
-          }
-
-          var line: String = ""
-          while (line != null) {
-            line = b.readLine()
-            if (line != null) {
-              logging.debug(s"Indexing log of Service(${_data.id}, ${_data.name}).")
-
-              logService.create(LogServiceModel(_data.id, _data.name, filename, line, System.currentTimeMillis()))
+                val fullpath = Paths.get(_data.logFileDirectory, filename)
+                val bufferedReader = new BufferedReader(new FileReader(fullpath.toFile))
+                files.addOne(filename -> bufferedReader)
+                var line: String = ""
+                while (line != null) {
+                  line = bufferedReader.readLine()
+                }
+                bufferedReader
             }
+
+            var line: String = ""
+            while (line != null) {
+              line = b.readLine()
+              if (line != null) {
+                logging.debug(s"Indexing log of Service(${_data.id}, ${_data.name}).")
+
+                logService.create(LogServiceModel(_data.id, _data.name, filename, line, System.currentTimeMillis()))
+              }
+            }
+          } else {
+            logging.debug(s"Filename $filename does not match the regex, skipping...")
           }
-        } else {
-          logging.debug(s"Filename $filename does not match the regex, skipping...")
         }
+      } catch {
+        case e: Exception =>
+          logging.error(s"Error collecting logs. Message: ${e.getMessage}")
       }
-    } catch {
-      case e: Exception =>
-        logging.error(s"Error collecting logs. Message: $e")
     }
   }
 
